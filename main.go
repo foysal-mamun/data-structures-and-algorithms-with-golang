263a25580274f8c40a8b774a90e7137ff4c17ac2
Foysal Mamun <sal_mdn@yahoo.com>
2015-07-22 15:37:12 +0600

    bulk commit

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/foysal-mamun/data-structures-and-algorithms-with-golang/helloWorld"
+	"github.com/foysal-mamun/data-structures-and-algorithms-with-golang/arrayFunctions"
 )
 
 func main() {
-	fmt.Println("Hello")
-	helloWorld.Message()
-	helloWorld.Message1()
+
+	fmt.Println(arrayFunctions.Frequency([]int{1, 23, 23, 2, 3, 3}))
 }
