3188fa11e858dd67cabd864d426c4e7be880c2d2
Foysal Mamun <sal_mdn@yahoo.com>
2015-09-22 04:34:29 +0600

    added triples 3 sum

--- maths/threesum.go
+++ maths/threesum.go
@@ -1,5 +1,9 @@
 package maths
 
+import (
+	"fmt"
+)
+
 type threesum struct {
 	nums []int
 }
@@ -10,9 +14,10 @@ func (ts *threesum) SumByTwoSum() map[int]int {
 	output := make(map[int]int)
 
 	twoSum := make(map[int]int)
-	for _, vi := range ts.nums {
-		for _, vj := range ts.nums {
-			twoSum[vi+vj] = vj
+	N := len(ts.nums)
+	for i := 0; i < N; i += 1 {
+		for j := i; j < N; j += 1 {
+			twoSum[ts.nums[i]+ts.nums[j]] = ts.nums[j]
 		}
 	}
 
@@ -24,4 +29,34 @@ func (ts *threesum) SumByTwoSum() map[int]int {
 	}
 
 	return output
+}
+
+func (ts *threesum) PrintAllThreeSum() {
+
+	N := len(ts.nums)
+	for i := 0; i < N; i += 1 {
+		for j := i; j < N; j += 1 {
+			for k := j; k < N; k += 1 {
+				if ts.nums[i]+ts.nums[j]+ts.nums[k] == 0 {
+					fmt.Println(ts.nums[i], ts.nums[j], ts.nums[k])
+				}
+			}
+		}
+	}
+}
+
+func (ts *threesum) TriplesCount() {
+	cnt := 0
+	N := len(ts.nums)
+	for i := 0; i < N; i += 1 {
+		for j := i; j < N; j += 1 {
+			for k := j; k < N; k += 1 {
+				if ts.nums[i]+ts.nums[j]+ts.nums[k] == 0 {
+					cnt += 1
+				}
+			}
+		}
+	}
+
+	fmt.Println("triples Count:: ", cnt)
 }
